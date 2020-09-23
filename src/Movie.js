26e8e14823f310bc2c27ac4e0b7e53e8f8b59180
junh0328 <junh0328@naver.com>
2020-09-23 18:11:13 +0900

    - Install gh-pages module seperate App.css & Movie.css

--- src/Movie.js
+++ src/Movie.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React from "react";
 import PropTypes from "prop-types";
-import "./App.css";
+import "./Movie.css";
 
-function Movie({ year, title, summary, poster, genres}){
+function Movie({ year, title, summary, poster, genres }) {
     return (
-    <div className="movie">
-        <img src={poster} alt={title} title={title}/>
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
             <div className="movie__data">
-            <h3 className="movie__title">{title}</h3>
-            <h5 className="movie__year">{year}</h5>
-            <ul className="genres">{genres.map((genre, index)=> 
-            <li key={index} className="genres__genre">{genre}</li> )}</ul>
-            <p className="movie__summary">{summary}</p>
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">
+                            {genre}
+                        </li>
+                    ))}
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 200)}...</p>
+            </div>
         </div>
-    </div>
     );
-};
+}
 
 Movie.propTypes = {
     id: PropTypes.number.isRequired,
@@ -27,4 +32,3 @@ Movie.propTypes = {
 };
 
 export default Movie;
-
