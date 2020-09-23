f0b4b98b757fd41ae4089fed798e1a10acd5785f
junh0328 <junh0328@naver.com>
2020-09-23 18:17:38 +0900

    Updates

---
+++ precache-manifest.71ccb35587a893ab251714f77034fff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2498ed1e9f886caa2b4d288edbe54596",
+    "url": "/movie_app__2020/index.html"
+  },
+  {
+    "revision": "7f7c0b4a51eb9fa01fd7",
+    "url": "/movie_app__2020/static/css/main.96ded0f3.chunk.css"
+  },
+  {
+    "revision": "0312909d6d349f431d4d",
+    "url": "/movie_app__2020/static/js/2.fcb93d28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app__2020/static/js/2.fcb93d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7c0b4a51eb9fa01fd7",
+    "url": "/movie_app__2020/static/js/main.8bdc2835.chunk.js"
+  },
+  {
+    "revision": "e80356d10ebbd416c378",
+    "url": "/movie_app__2020/static/js/runtime-main.fee0d2ad.js"
+  }
+]);
