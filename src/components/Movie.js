8225bef357b57fa1b57f59e98d40f70e3ab5fb11
junh0328 <junh0328@naver.com>
2020-09-27 22:49:56 +0900

    className="movie" 의 위치 변경 <Link> 태그 오류 해결 -마무리

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,17 +5,19 @@ import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
     return (
-        <Link to={{
-            pathname: `/movie/${id}`,
-            state: {
-                year,
-                title,
-                summary,
-                poster,
-                genres
-            }
-        }}>
-            <div className="movie">
+        <div className="movie">
+            <Link
+                to={{
+                    pathname: `/movie/${id}`,
+                    state: {
+                        year,
+                        title,
+                        summary,
+                        poster,
+                        genres
+                    }
+                }}
+            >
                 <img src={poster} alt={title} title={title} />
                 <div className="movie__data">
                     <h3 className="movie__title">{title}</h3>
@@ -27,14 +29,15 @@ function Movie({ id, year, title, summary, poster, genres }) {
                             </li>
                         ))}
                     </ul>
-                    <p className="movie__summary">{summary.slice(0, 200)}...</p>
+                    <p className="movie__summary">{summary.slice(0, 180)}...</p>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 }
 
 Movie.propTypes = {
+    id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
